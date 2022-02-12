ce9f4bb0e8d9bf5f21200c7536e40c544d5dc111
Xayrulloh <xayrullohabduvohidov713@gmail.com>
2022-02-12 19:52:08 +0500

    little changes

--- repeat.js
+++ repeat.js
@@ -1,3 +1,19 @@
 console.clear()
+// console.log('This is \n program');
+
+// let x=1, y=2,
+// z=
+// x+y;
+// console.log(x, y, z);
+
+// Netscape is the software company that developed JavaScript.
+
+// console.log(parseInt ("4F", 16));
+
+// console.log(3 + 2 ++'5');
+
+
+
+
 
 
