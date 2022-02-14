b025cfa72a90907bb5d1fc7528e51c3406d2a08e
Xayrulloh <xayrullohabduvohidov713@gmail.com>
2022-02-14 11:31:05 +0500

    updated

--- repeat.js
+++ repeat.js
@@ -12,6 +12,125 @@ console.clear()
 
 // console.log(3 + 2 ++'5');
 
+// console.log(+null+ + - + +'4');
+
+// let name = 'Xayrulloh'
+
+// function getName() {
+//     console.log(name);
+//     name = 'Sayfulloh'
+// }
+// getName()
+
+// let number = 25
+
+// var main = function() {
+//     console.log(number);
+//     number = 40
+// }
+// main()
+
+// function addToList(item, list) {
+//     return list.push(item)
+// }
+
+// const res = addToList('apple', ['banana'])
+// console.log(res);
+
+// let num = 10
+
+// const increaseNumber = () => num++
+// const increasePassedNumber = number => number++
+
+// const num1 = increaseNumber()
+// const num2 = increasePassedNumber(num1)
+
+// console.log(num1);
+// console.log(num2);
+
+// setInterval(() => console.log('hi'), 1000)
+
+// console.log(`${(x => x)('I love')} to program`);
+
+// console.log(Array.isArray({length : 5}));
+
+// console.log(6 * '2');
+
+// if (102 & 1) {
+//     console.log('yeah');
+// }
+
+// function a () {
+//     let c = b = 33
+// }
+// a()
+// console.log(b);
+// console.log(global);
+
+// "use strict"
+// eval('var x = 1;')
+// console.log(x);
+
+// let apt = 2
+// apt = apt << 4
+// console.log(apt);
+
+// function compareMembers(person1, person2 = person) {
+//     console.log(person1, person2);
+//     if (person1 !== person2) {
+//         console.log('not the same');
+//     } else {
+//         console.log('they are same');
+//     }
+// }
+
+// let person = {name : 'Xayrulloh'}
+// compareMembers(person)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
